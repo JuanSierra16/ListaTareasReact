ab53d880bf6cff09ffc7e166a38aa34192d7e36a
Juan Jose <juanjose.sierra@utp.edu.co>
2023-06-29 18:12:35 -0500

    Lista de Tareas terminada

---
+++ src/componentes/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+
+const Header = ({mostrarCompletadas, cambiarMostrarCompletadas}) => {
+    return ( 
+        <header className='header'>
+            <h1 className='header__titulo'>Lista de Tareas</h1>
+            <button className='header__boton' onClick={() => cambiarMostrarCompletadas(!mostrarCompletadas)}>
+                {mostrarCompletadas ? 
+                <>
+                    No mostrar completadas
+                    <FontAwesomeIcon icon={faEyeSlash} className='header__icono-boton' />
+                </>
+                : 
+                <>
+                    Mostrar Completadas
+                    <FontAwesomeIcon icon={faEye} className='header__icono-boton' />
+                </>
+                }
+            </button>
+        </header>
+     );
+}
+ 
+export default Header;
